480b921c019ca15d7f784d73eca1a63ba02d66b0
Onofre Souza <souzaonofre@gmail.com>
2025-12-10 18:41:30 -0300

    make basic crud api resources to Link and Action domains

---
+++ backend/app/Http/Controllers/ActionController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreActionRequest;
+use App\Http\Requests\UpdateActionRequest;
+use App\Models\Action;
+
+class ActionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreActionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Action $action)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateActionRequest $request, Action $action)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Action $action)
+    {
+        //
+    }
+}
